a18b1ed34d859d992f3ba639cb65ec78662c1f02
themafia98 <gaara33377@gmail.com>
2020-01-09 22:30:55 +0300

    run procedure

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,4 +1,38 @@
 
 module.exports = function(route, db){
-    
+    route.get("/rangeTemp", async (req, res) => {
+        try {
+        const connect = await db.connect();
+        if (!connect) return res.sendStatus(503);
+
+        
+        // connect.input('input_parameter', sql.int, value)
+        //     .output('output_parameter', sql.VarChar(50))
+        //     .execute('procedure_name');
+        db.close();
+
+        } catch(err){
+            console.err(err);
+            if (res.headersSent) res.sendStatus(500);
+        }
+    });
+
+    route.get("/calcTemp", async (req, res) => {
+        try {
+        const connect = await db.connect();
+        if (!connect) return res.sendStatus(503);
+
+        const calcResult = await connect.request().execute("GET_CALC");
+        const isValid = calcResult && Array.isArray(calcResult.recordset) && calcResult.recordset.length;
+        db.close();
+        
+        
+        if (isValid) return res.status(200).json(JSON.stringify(calcResult.recordset[0]));
+        else return res.sendStatus(404);
+        
+        } catch(err){
+            console.error(err);
+            if (res.headersSent) res.sendStatus(500);
+        }
+    });
 }
